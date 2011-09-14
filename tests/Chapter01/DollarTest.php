ba337d2706f026138a78996f589c9d5665dbbe16
k_somemo <kntsmy@hotmail.co.jp>
2011-09-14 15:36:05 +0900

    Dollar Class add and TestCase edit

--- tests/Chapter01/DollarTest.php
+++ tests/Chapter01/DollarTest.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once '/src/Chapter01/Dollar.php';
+
 class DollarTest extends PHPUnit_Framework_TestCase {
 
     /**
@@ -6,10 +9,9 @@ class DollarTest extends PHPUnit_Framework_TestCase {
      * 1口当たりの価格に口数を掛けたときの金額
      */
     public function testMultiplication() {
-        $fives = new Dollar();
+        $fives = new Dollar(5);
         $fives->times(2);
         assertEquals(10, $fives->amount, "1口当たりの価格に口数を掛けたときの金額が期待通りでない");
     }
-
 
 }
