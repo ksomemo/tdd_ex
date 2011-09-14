5f279ccf7240c7f6e17ecd36a5c51ba0251cd4e8
k_somemo <kntsmy@hotmail.co.jp>
2011-09-14 15:44:34 +0900

    Dollar#times method add and Assert call edit

--- src/Chapter01/Dollar.php
+++ src/Chapter01/Dollar.php
@@ -1,4 +1,12 @@
 <?php
 class Dollar {
 
+    /**
+     *
+     * 価格に口数をかけて金額を変更する
+     * @param int $multiplier 口数
+     */
+    public function times($multiplier) {
+        ;
+    }
 }
--- tests/Chapter01/DollarTest.php
+++ tests/Chapter01/DollarTest.php
@@ -11,7 +11,7 @@ class DollarTest extends PHPUnit_Framework_TestCase {
     public function testMultiplication() {
         $fives = new Dollar(5);
         $fives->times(2);
-        assertEquals(10, $fives->amount, "1口当たりの価格に口数を掛けたときの金額が期待通りでない");
+        $this->assertEquals(10, $fives->amount, "1口当たりの価格に口数を掛けたときの金額が期待通りでない");
     }
 
 }
