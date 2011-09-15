5d1c7b85dc3a323bd371962ee1e896067b2cca4c
k_somemo <kntsmy@hotmail.co.jp>
2011-09-15 12:44:09 +0900

    refactoring add contsructor and times method

--- src/Chapter01/Dollar.php
+++ src/Chapter01/Dollar.php
@@ -3,12 +3,16 @@ class Dollar {
 
     public $amount;
 
+    public function __construct($amount) {
+        $this->amount = $amount;
+    }
+
     /**
      *
      * 価格に口数をかけて金額を変更する
      * @param int $multiplier 口数
      */
     public function times($multiplier) {
-        $this->amount = 5 * $multiplier;
+        $this->amount *= $multiplier;
     }
 }
