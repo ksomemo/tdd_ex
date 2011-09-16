d5af1e6e79ab204f56835d7dbc2c888d3f0fec2b
k_somemo <kntsmy@hotmail.co.jp>
2011-09-16 18:01:28 +0900

    add testcase amount expected 15

--- tests/Chapter01/DollarTest.php
+++ tests/Chapter01/DollarTest.php
@@ -12,6 +12,10 @@ class DollarTest extends PHPUnit_Framework_TestCase {
         $fives = new Dollar(5);
         $fives->times(2);
         $this->assertEquals(10, $fives->amount, "1口当たりの価格に口数を掛けたときの金額が期待通りでない");
+
+        $fives->times(3);
+        $this->assertEquals(15, $fives->amount, "金額が期待通り(15)でない");
+
     }
 
 }
